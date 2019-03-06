96b7b591afcf8672fd12cc39e530d868cb6df8ea
¨cliffrpi¨ <clazzasaur@gmail.com>
2019-03-06 02:32:34 +0000

    Fixed all the things.

--- dfrobot_touch_sensor.py
+++ dfrobot_touch_sensor.py
@@ -1,16 +1,16 @@
 # DFROBOT TOUCH SENSOR V2
 # GPIO 26 on the green connecetor. 3.3/5v on the red, gnd on black.
+# the PCB already has the appropriate resistors.
 
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(26, GPIO.IN) # setup as an input (we want to detect a HIGH)
 
 while True:
     state = GPIO.input(26)
-    print(state)
-    if state == False:
-        print('Button Pressed')
+    if state == True: # so when a high is pressed...
+        print('Surface pressed')
         time.sleep(0.2)
